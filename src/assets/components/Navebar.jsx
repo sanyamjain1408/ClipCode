831b0fb6cf4a4f78381b619a533afb63a6084f96
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-04 01:27:44 +0530

    css changes

--- src/assets/components/Navebar.jsx
+++ src/assets/components/Navebar.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 const Navebar = () => {
   return (
-    <div className='flex flex-row justify-evenly items-center  gap-4 place-content-evenly bg-white h-[100px] w-[100%]'>
+    <div className='flex flex-row justify-evenly items-center  gap-4 place-content-evenly p-0 m-0 rounded bg-white h-[100px] w-[800px]'>
       <NavLink
       to = "/"
       >
-        Home 
+        Home
       </NavLink>
 
       <NavLink

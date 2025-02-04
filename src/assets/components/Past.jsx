d8afe0e186f3afad4123e3709c6d6dd7270ef8d4
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-04 15:30:09 +0530

    changes in tailwind

--- src/assets/components/Past.jsx
+++ src/assets/components/Past.jsx
@@ -36,7 +36,7 @@ const Past = () => {
       <div id='sam' className='flex justify-center'>
       <input
       id='sanyam'
-        className='  p-3 rounded-2xl w-[90%] md:w-[50vw] mt-5 bg-black text-left placeholder-gray-400 placeholder-blink '
+        className='  p-5 rounded-2xl w-[90%] md:w-[50vw] mt-5 bg-black text-left placeholder-gray-400 placeholder-blink '
         type='search'
         placeholder ='search here .....'
         value={searchTerm}
@@ -45,11 +45,11 @@ const Past = () => {
       </div>
       
       <div className='flex justify-center  mt-5 '>
-      <div className='flex flex-col gap-5 w-full max-w-screen-lg'>
+      <div className='flex flex-col gap-4 w-full max-w-screen-lg'>
         {filteredData.length > 0 && filteredData.map((paste) => {
           return (
             
-            <div key={paste._id} className=' flex justify-between p-5 gap-1.5 border  w-full rounded-xl '>
+            <div key={paste._id} className=' flex justify-between p-3 gap-1.5 border  w-full rounded-xl '>
 
 
               <div >
